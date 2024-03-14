15c6bf78ece3152da298110cb57080e9fac0d143
CeccoBolt <cecchinimichael.code@gmail.com>
2024-03-14 08:53:38 +0100

    Correct script

--- src/components/Skills/script.js
+++ src/components/Skills/script.js
@@ -1,22 +1,24 @@
-window.onload = function () {
-    const cards = Array.from(document.querySelectorAll(".skills-card"));
-    const cardsContainer = document.querySelector(".skills-cards");
+if (typeof window !== "undefined") {
+    window.onload = function () {
+        const cards = Array.from(document.querySelectorAll(".skills-card"));
+        const cardsContainer = document.querySelector(".skills-cards");
 
-    var x, y;
-    var animation = "running";
-    cardsContainer.addEventListener("click", (e) => {
-        animation = animation === "running" ? "paused" : "running";
-        cardsContainer.style.setProperty("animation-play-state", animation);
-    });
+        var x, y;
+        var animation = "running";
+        cardsContainer.addEventListener("click", (e) => {
+            animation = animation === "running" ? "paused" : "running";
+            cardsContainer.style.setProperty("animation-play-state", animation);
+        });
 
-    cardsContainer.addEventListener("mousemove", (e) => {
-        for (const card of cards) {
-            const rect = card.getBoundingClientRect();
-            x = e.clientX - rect.left;
-            y = e.clientY - rect.top;
+        cardsContainer.addEventListener("mousemove", (e) => {
+            for (const card of cards) {
+                const rect = card.getBoundingClientRect();
+                x = e.clientX - rect.left;
+                y = e.clientY - rect.top;
 
-            card.style.setProperty("--mouse-x", `${x}px`);
-            card.style.setProperty("--mouse-y", `${y}px`);
-        }
-    });
+                card.style.setProperty("--mouse-x", `${x}px`);
+                card.style.setProperty("--mouse-y", `${y}px`);
+            }
+        });
+    }
 }
