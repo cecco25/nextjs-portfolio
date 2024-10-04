9f97730a28e08842b0faba076928e1db6936af54
CeccoBolt <cecchinimichael.code@gmail.com>
2024-10-04 12:10:20 +0200

    Create About Me

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/navbar";
-import Intro from "@/components/Intro";
+import Intro from "@/components/intro";
+import About from "@/components/about";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <Intro />
+      <About />
     </main>
   );
 }
